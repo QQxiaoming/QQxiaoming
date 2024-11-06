7d841be1aaef04df28cf08b414fc3185081a433a
xiaoming <2014500726@smail.xtu.edu.cn>
2024-11-06 17:38:53 +0800

    update data
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- parse_duolingo_svg.py
+++ parse_duolingo_svg.py
@@ -2,19 +2,11 @@
 # 本文件在 copilot 生成的代码基础上修改而来，全程没有搜索引擎的帮助，只是通过阅读代码，自己理解，然后修改而来
 import xml.etree.ElementTree as ET
 import sys
-import os
 import time
-import json
 
 append_list = [
     # add your data here
-    ["2023-03-26", 450,],  # sunday
-    ["2023-03-27", 94,],   # monday
-    ["2023-03-28", 182,],  # tuesday
-    # ["2023-03-29", 0,],  # wednesday
-    # ["2023-03-30", 0,],  # thursday
-    # ["2023-03-31", 0,],  # friday
-    # ["2023-04-01", 0,],  # saturday
+    ["2024-11-05", 20 ],
 ]
 
 def get_color(xp):
@@ -42,21 +34,30 @@ def parse_svg(svg_file):
 
     # 2022 year data output
     sum_xp_2022 = 0
-    for i in range(20+365+17, 20+365+17+365):
+    for i in range(20+365+18+365+17, 20+365+18+365+17+365):
         data = root[i][0].text.split(' ')
         if(len(data) == 3):
             root[i].attrib['fill'] = get_color(int(data[1]))
             print(data[0],data[1]+"XP")
             sum_xp_2022 += int(data[1])
 
-    # 2023 year data output, support check and append data
+    # 2023 year data output
     sum_xp_2023 = 0
-    for i in range(20, 20+365):
+    for i in range(20+365+18, 20+365+18+365):
         data = root[i][0].text.split(' ')
         if(len(data) == 3):
             root[i].attrib['fill'] = get_color(int(data[1]))
             print(data[0],data[1]+"XP")
             sum_xp_2023 += int(data[1])
+
+    # 2024 year data output, support check and append data
+    sum_xp_2024 = 0
+    for i in range(20, 20+365):
+        data = root[i][0].text.split(' ')
+        if(len(data) == 3):
+            root[i].attrib['fill'] = get_color(int(data[1]))
+            print(data[0],data[1]+"XP")
+            sum_xp_2024 += int(data[1])
         if(len(data) == 1):
             if(time.strptime(data[0], "%Y-%m-%d") < time.localtime()):
                 if(time.strftime("%Y-%m-%d", time.localtime()) != data[0]):
@@ -66,12 +67,13 @@ def parse_svg(svg_file):
                             if(append_list[j][0] == data[0]):
                                 root[i][0].text = data[0]+" "+str(append_list[j][1])+" XP"
                                 root[i].attrib['fill'] = get_color(append_list[j][1])
-                                sum_xp_2023 += append_list[j][1]
+                                sum_xp_2024 += append_list[j][1]
     
-    print("sum_xp_2022:", sum_xp_2022, "svg:" ,root[20+365+1].text)
-    print("sum_xp_2023:", sum_xp_2023, "svg:" ,root[4].text)
-    if(root[4].text != str(sum_xp_2023)+" XP"):
-        root[4].text = str(sum_xp_2023)+" XP"
+    print("sum_xp_2022:", sum_xp_2022, "svg:" ,root[4+365+18+365+17].text)
+    print("sum_xp_2023:", sum_xp_2023, "svg:" ,root[4+365+18].text)
+    print("sum_xp_2024:", sum_xp_2024, "svg:" ,root[4].text)
+    if(root[4].text != str(sum_xp_2024)+" XP"):
+        root[4].text = str(sum_xp_2024)+" XP"
 
     xml.write(file_or_filename=svg_file, encoding="utf-8", xml_declaration=None, default_namespace=None, method="xml")
 
@@ -81,3 +83,4 @@ if __name__ == '__main__':
         sys.exit(1)
     svg_file = sys.argv[1]
     parse_svg(svg_file)
+
